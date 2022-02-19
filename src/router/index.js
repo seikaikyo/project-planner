06cd72ac612639e0d03964bbfeb78cca360f7b93
Yoshika <phpbbtw@gmail.com>
2022-02-20 00:37:23 +0800

    Update

--- src/router/index.js
+++ src/router/index.js
@@ -1,7 +1,6 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import Home from '../views/Home.vue'
 import AddProject from '../views/AddProject.vue'
-import EditProject from '../views/EditProject.vue'
 
 const routes = [
   {
@@ -13,12 +12,6 @@ const routes = [
     path: '/add',
     name: 'AddProject',
     component: AddProject
-  },
-  {
-    path: '/projects/:id',
-    name: 'EditProject',
-    component: EditProject,
-    props: true
   }
 ]
 
